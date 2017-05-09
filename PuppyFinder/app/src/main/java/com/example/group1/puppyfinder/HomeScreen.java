73b60d4ec9825c0ffd1263ac4bbc969628ea02e5
Grace Chandler <gchan1@umbc.edu>
2017-05-09 00:47:59 -0400

    Added icon and colors

--- PuppyFinder/app/src/main/java/com/example/group1/puppyfinder/HomeScreen.java
+++ PuppyFinder/app/src/main/java/com/example/group1/puppyfinder/HomeScreen.java
@@ -1,53 +1,38 @@
 package com.example.group1.puppyfinder;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class HomeScreen extends AppCompatActivity implements View.OnClickListener{
 
-    private TextView mTextMessage;
-    Button buttonShelters, buttonEvents;
+    //private TextView mTextMessage;
+    public ImageButton buttonShelters, buttonEvents, buttonMaps;
+    public View topBar;
     Intent intent;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_home_screen);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        buttonShelters = (Button) findViewById(R.id.buttonShelters);
-        buttonEvents = (Button) findViewById(R.id.buttonEvents);
+        topBar = (View) findViewById(R.id.topBar);
+        buttonShelters = (ImageButton) findViewById(R.id.buttonShelters);
+        buttonEvents = (ImageButton) findViewById(R.id.buttonEvents);
+        buttonMaps = (ImageButton) findViewById(R.id.buttonMap) ;
+        buttonMaps.bringToFront();
+        buttonEvents.bringToFront();
+        buttonShelters.bringToFront();
         buttonShelters.setOnClickListener(this);
         buttonEvents.setOnClickListener(this);
     }
